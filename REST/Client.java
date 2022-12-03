f1eb03f01f266b9664874c7b0f36e6240139cf17
Wex <bernikcooldude@yandex.ru>
2022-12-03 13:40:09 +0300

    rest code

---
+++ REST/Client.java
@@ -0,0 +1,41 @@
+package com.restful.app.restexampleapp.model;
+
+public class Client {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
